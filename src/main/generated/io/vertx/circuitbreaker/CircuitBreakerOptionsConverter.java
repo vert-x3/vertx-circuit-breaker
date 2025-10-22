dd57766097bd9484fc21a63f4660f0ba09515fb4
Julien Viet <julien@julienviet.com>
2025-10-22 12:41:12 +0000

    Set next snapshot version

--- src/main/generated/io/vertx/circuitbreaker/CircuitBreakerOptionsConverter.java
+++ src/main/generated/io/vertx/circuitbreaker/CircuitBreakerOptionsConverter.java
@@ -2,6 +2,8 @@ package io.vertx.circuitbreaker;
 
 import io.vertx.core.json.JsonObject;
 import io.vertx.core.json.JsonArray;
+import java.time.Instant;
+import java.time.format.DateTimeFormatter;
 
 /**
  * Converter and mapper for {@link io.vertx.circuitbreaker.CircuitBreakerOptions}.
